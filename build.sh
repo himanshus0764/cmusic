ef334bcffd3bd5adcdc7a97d8b6b53ae16d0d52b
Jagannath Hari <Jagannathhari17@gmail.com>
2025-01-20 23:55:41 +0530

    Updated build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ CC="gcc"
 CFLAGS="-lGL -lm -lpthread -ldl -lrt -lX11"
 $CC  -o $PROJECT_DIR/$APP_NAME $PROJECT_DIR/main.c ./libraylib.a ./libfftw3.a ./libfftw3f.a -I. $CFLAGS
 # ./music_player /home/laptop_asus/Music/music
-./music_player /home/laptop_asus/Downloads
+# ./music_player /home/laptop_asus/Downloads
